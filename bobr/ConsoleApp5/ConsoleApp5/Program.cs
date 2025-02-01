90f9b442bc7818da5bd677984f8809322aa04c64
oleksii103 <132712296+oleksii103@users.noreply.github.com>
2025-02-01 14:44:21 +0200

    sdfgh

---
+++ bobr/ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter math expration");
+            string expration = Console.ReadLine();
+            string[] mark = expration.Split('*');
+            int result = 1;
+            for (int i = 0; i < mark.Length; i++) 
+            {
+                result = int.Parse(mark[i])*result;
+            }
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}

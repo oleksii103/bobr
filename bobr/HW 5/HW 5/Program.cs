4127ef284b74aca92308a827b22f09e94ae563a9
oleksii103 <132712296+oleksii103@users.noreply.github.com>
2025-02-24 18:55:30 +0200

    home work 5

---
+++ bobr/HW 5/HW 5/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TicTacToe
+{
+    class Program
+    {
+        static Dictionary<char, string> morseCode = new Dictionary<char, string>
+        {
+            {'A', ".-"}, {'B', "-..."}, {'C', "-.-."}, {'D', "-.."}, {'E', "."},
+            {'F', "..-."}, {'G', "--."}, {'H', "...."}, {'I', ".."}, {'J', ".---"},
+            {'K', "-.-"}, {'L', ".-.."}, {'M', "--"}, {'N', "-."}, {'O', "---"},
+            {'P', ".--."}, {'Q', "--.-"}, {'R', ".-."}, {'S', "..."}, {'T', "-"},
+            {'U', "..-"}, {'V', "...-"}, {'W', ".--"}, {'X', "-..-"}, {'Y', "-.--"}, {'Z', "--.."},
+            {'1', ".----"}, {'2', "..---"}, {'3', "...--"}, {'4', "....-"}, {'5', "....."},
+            {'6', "-...."}, {'7', "--..."}, {'8', "---.."}, {'9', "----."}, {'0', "-----"},
+            {' ', " / "}
+        };
+        static char[,] board = {
+            { '1', '2', '3' },
+            { '4', '5', '6' },
+            { '7', '8', '9' }
+        };
+        static char player = 'X', computer = '*';
+        static Random rand = new Random();
+
+        static void Main(string[] args)
+        {
+            /*task 1
+            Console.WriteLine("Welcome to 'Tic-Tac-Toe'!");
+            bool playerStarts = rand.Next(2) == 0;
+            Console.WriteLine(playerStarts ? "The player goes first!" : "The computer goes first!");
+
+            while (true)
+            {
+                PrintBoard();
+                if (playerStarts)
+                {
+                    PlayerMove();
+                    if (CheckWin(player)) { PrintBoard(); Console.WriteLine("You win!"); break; }
+                    playerStarts = false;
+                }
+                else
+                {
+                    ComputerMove();
+                    if (CheckWin(computer)) { PrintBoard(); Console.WriteLine("The computer wins!"); break; }
+                    playerStarts = true;
+                }
+
+                if (IsBoardFull()) { PrintBoard(); Console.WriteLine("It's a draw!"); break; }
+            }
+        }
+
+        static void PrintBoard()
+        {
+            Console.Clear();
+            Console.WriteLine(" {0} | {1} | {2} ", board[0, 0], board[0, 1], board[0, 2]);
+            Console.WriteLine("---+---+---");
+            Console.WriteLine(" {0} | {1} | {2} ", board[1, 0], board[1, 1], board[1, 2]);
+            Console.WriteLine("---+---+---");
+            Console.WriteLine(" {0} | {1} | {2} ", board[2, 0], board[2, 1], board[2, 2]);
+        }
+
+        static void PlayerMove()
+        {
+            int move;
+            while (true)
+            {
+                Console.Write("Your move (1-9): ");
+                if (int.TryParse(Console.ReadLine(), out move) && move >= 1 && move <= 9 && IsCellEmpty(move))
+                {
+                    PlaceMove(move, player);
+                    break;
+                }
+                Console.WriteLine("Invalid move, try again.");
+            }
+        }
+
+        static void ComputerMove()
+        {
+            int move;
+            do { move = rand.Next(1, 10); } while (!IsCellEmpty(move));
+            PlaceMove(move, computer);
+            Console.WriteLine("The computer chose {0}", move);
+        }
+
+        static bool IsCellEmpty(int move)
+        {
+            int row = (move - 1) / 3, col = (move - 1) % 3;
+            return board[row, col] != player && board[row, col] != computer;
+        }
+
+        static void PlaceMove(int move, char symbol)
+        {
+            int row = (move - 1) / 3, col = (move - 1) % 3;
+            board[row, col] = symbol;
+        }
+
+        static bool CheckWin(char symbol)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                if ((board[i, 0] == symbol && board[i, 1] == symbol && board[i, 2] == symbol) ||
+                    (board[0, i] == symbol && board[1, i] == symbol && board[2, i] == symbol))
+                    return true;
+            }
+            return (board[0, 0] == symbol && board[1, 1] == symbol && board[2, 2] == symbol) ||
+                   (board[0, 2] == symbol && board[1, 1] == symbol && board[2, 0] == symbol);
+        }
+
+        static bool IsBoardFull()
+        {
+            foreach (char c in board)
+                if (c != player && c != computer)
+                    return false;
+            return true;*/
+
+            /*task 3
+            Console.WriteLine("Enter text to translate to Morse Code:");
+            string input = Console.ReadLine().ToUpper();
+            string morseTranslation = TranslateToMorse(input);
+            Console.WriteLine("Morse Code:");
+            Console.WriteLine(morseTranslation);
+            */
+        }
+        /*for task 3
+        static string TranslateToMorse(string text)
+        {
+            string result = "";
+            foreach (char c in text)
+            {
+                if (morseCode.ContainsKey(c))
+                {
+                    result += morseCode[c] + " ";
+                }
+            }
+            return result.Trim();
+        }
+        */
+    }
+}
---
+++ bobr/bambuk/bambuk/Program.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Dynamic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bambuk
+{
+    internal class Program
+    {
+        public static IEnumerable<T> FilterByTwoCriteria<T>(IEnumerable<T> Yetty, Func<T, bool> Pred1, Func<T, bool> Pred2)
+        {
+            List<T> list = new List<T>();
+            for (int i = 0; i < Yetty.Count(); i++)
+            {
+                if (Pred1(Yetty.ElementAt(i)) && Pred2(Yetty.ElementAt(i)))
+                {
+                    list.Add(Yetty.ElementAt(i));
+                    
+                }
+            }
+            return list;
+        }
+
+        public static T GenMin<T>(T n1, T n2, T n3) where T : IComparable<T>
+        {
+            T MinN = n1;
+            if (MinN.CompareTo(n2) == 1)
+            {
+                MinN = n2;
+                if (MinN.CompareTo(n3) == 1)
+                {
+                    MinN = n3;
+                }
+            }
+            else if (MinN.CompareTo(n3) == 1)
+            {
+                MinN = n3;
+
+            }
+            return MinN;
+        }
+        public static int MinNumber (int n1, int n2, int n3)
+        {
+            int MinN = n1;
+            if (MinN > n2)
+            {
+                MinN = n2;
+                if (MinN > n3)
+                {
+                    MinN = n3;
+                }
+            }
+            else if (MinN > n3)
+            {
+                MinN = n3;
+
+            }
+            return MinN;
+        }
+        static void Main(string[] args)
+        {
+            /*task Markian
+                int size = Int32.Parse(Console.ReadLine());
+                int num;
+                int[] arr = new int[size];
+
+                for (int i = 0; i < size; i++)
+                {
+                    num = Int32.Parse(Console.ReadLine());
+                    arr[i] = num;
+                }
+
+                Func<int[], (int, int)> max = (arr2) =>
+                {
+                    int maxElem = 0;
+                    int Index = 0;
+                    for (int i = 0; i < arr2.Length; i++)
+                    {
+                        if (arr2[i] > maxElem)
+                        {
+                            Index = i;
+                            maxElem = arr2[i];
+                        }
+
+                    }
+                    return (Index, maxElem);
+                };
+
+                var result = max(arr);
+
+                Console.WriteLine(result);
+                Console.ReadLine();
+           */
+
+            /*task Nazar Kitchak
+            Func<int, string> biber = (number) =>
+            {
+                if (number % 2 == 0) return "parne";
+
+                else return "neparne";
+                         
+            };
+            Console.WriteLine(biber(2));
+            Console.WriteLine(biber(5));
+
+            int kf = Int32.Parse(Console.ReadLine());
+            Console.WriteLine(biber(kf));
+            Console.ReadLine();
+            */
+
+            /*task Matviy 
+             * Func<int, int, double> bubu = (x, y) => Math.Pow(x, y);
+
+            Console.WriteLine(bubu(2, 32));
+            Console.ReadLine();
+            */
+
+            /*Mein Task (task 7)
+            Console.Write("Enter arr size: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int[] arr = new int[n];
+
+            Console.WriteLine("Enter arr element:");
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+
+            var oddNumbers = arr.Where(num => num % 2 != 0).ToArray();
+
+            Console.WriteLine("Neparne: ");
+            Console.WriteLine(string.Join(", ", oddNumbers)); 
+            */
+
+            /*task generik Min
+            Console.WriteLine(MinNumber(1, 2, 3));
+            Console.WriteLine(MinNumber(2, 3, 1));
+            Console.WriteLine(MinNumber(3, 1, 2));
+            Console.WriteLine(MinNumber(3, 2, 1));
+
+            Console.WriteLine("enother method");
+            Console.WriteLine(GenMin<int>(5, 2, 97));
+            */
+            List<int> list = new List<int>() { 1, 4, 90, 567, 3456, 2 };
+             
+            List<int>NList = FilterByTwoCriteria<int>(list, num => num % 2 == 0, num => num > 0 && num < 10).ToList();
+            for (int i = 0; i < NList.Count; i++)
+            {
+                Console.WriteLine(NList[i]);
+            }
+            List<String> strings = new List<String>() { "ass", "anatolii", "mhgfr", "lkjhgfds", "add" };
+            List<string> SList = FilterByTwoCriteria<string>(strings, str=> str.Length <= 3, str=> str.StartsWith("a")).ToList();
+            for (int i = 0; i < SList.Count; i++)
+            {
+                Console.WriteLine(SList[i]);
+            }
+        }
+    }
+}
